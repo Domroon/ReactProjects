75327f0fccb93d5265bdff722efb3c75fb1212c1
Dominik Haeusser <haeusser-dominik@web.de>
2021-09-09 12:13:34 +0200

    add some css in jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import ReactDom from "react-dom"
+import "./index.css"
 
 function BookList() {
   return (
-    <section>
-      <Book />
+    <section className="booklist">
       <Book />
     </section>
   )
@@ -12,7 +12,7 @@ function BookList() {
 
 const Book = () => {
   return (
-    <article>
+    <article className="book">
       <Image />
       <Title />
       <Author />
@@ -22,6 +22,6 @@ const Book = () => {
 
 const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/91r4uIYku9S._AC_UL200_SR200,200_.jpg" alt="" />
 const Title = () => <h1>Das Café am Rande der Welt</h1>
-const Author = () => <h4>John Strelecky</h4>
+const Author = () => <h4 style={{ color: "617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>John Strelecky</h4>
 
 ReactDom.render(<BookList />, document.getElementById("root"))
