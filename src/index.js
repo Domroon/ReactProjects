8ea2fb6f38b88d2f862fc3a2fb7f5d28e9ddc4ab
Dominik Haeusser <haeusser-dominik@web.de>
2021-09-09 09:33:10 +0200

    add simple components and render it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import React from "react"
+import ReactDom from "react-dom"
 
+// stateless functional component
+// always return JSX
+
+function Greeting() {
+  return (
+    <>
+      <Person />
+      <p>Hey whats up?!!!!!</p>
+      <Message />
+    </>
+  )
+}
+
+const Person = () => <h1>Hello World!</h1>
+const Message = () => <p>I'm fine and you?</p>
+
+ReactDom.render(<Greeting />, document.getElementById("root"))
