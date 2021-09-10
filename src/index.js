ace71fc7e27fffb77f6eedb96eaf148a319cba3c
Dominik Haeusser <haeusser-dominik@web.de>
2021-09-10 09:11:22 +0200

    add onClick and mouseOver Events

--- src/index.js
+++ src/index.js
@@ -45,8 +45,8 @@ function BookList() {
       </section>
       <section className="booklist">
         {books.map((book) => {
-          const { title, author, img, id } = book
-          return <Book key={id} title={title} author={author} img={img} />
+          //const { title, author, img, id } = book
+          return <Book {...book} />
         })}
       </section>
     </>
@@ -57,15 +57,37 @@ const Book = ({ img, title, author, job, children }) => {
   const item = "book"
   // const { img, title, author} = props  That can also destruct props
 
+  //Events
+  // atrribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert("Hello World")
+  }
+  const complexExample = (author) => {
+    console.log(author)
+  }
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title)
+      }}
+    >
       <Image img={img} />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log({ title })}>{title}</h1>
       <h4 style={{ color: "617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>{author}</h4>
       <p>{item}</p>
       <p>{6 + 6}</p>
       <p>{job}</p>
       <p>{children}</p>
+      <button type="button" onClick={clickHandler}>
+        Test
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More complex example
+      </button>
     </article>
   )
 }
