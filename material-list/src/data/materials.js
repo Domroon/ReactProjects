8794de914be964a0f79eb7ad6538af8ffcf47e04
Dominik Haeusser <haeusser-dominik@web.de>
2021-10-11 05:39:06 +0200

    add materials

---
+++ material-list/src/data/materials.js
@@ -0,0 +1,22 @@
+export const materials = [
+  {
+    number: 0,
+    mat_number: 222122,
+    mat_name: "banana",
+  },
+  {
+    number: 0,
+    mat_number: 147258,
+    mat_name: "strawberry",
+  },
+  {
+    number: 0,
+    mat_number: 953468,
+    mat_name: "apple",
+  },
+  {
+    number: 0,
+    mat_number: 953761,
+    mat_name: "onion",
+  },
+]
