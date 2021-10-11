5a5de72e73096e9e8edb2a887ede6201a5d36ec2
Dominik Haeusser <haeusser-dominik@web.de>
2021-10-11 05:40:28 +0200

    add the components

--- material-list/src/App.js
+++ material-list/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
+import FindItem from "./components/FindItem"
+import NewMaterial from "./components/NewMaterial"
+import List from "./components/List"
+import Alert from "./components/Alert"
+import { materials } from "./data/materials"
 
 function App() {
+  const [items, setItems] = useState(materials)
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" })
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="alert-container">
+        {alert.show && (
+          <Alert {...alert} removeAlert={showAlert} items={items} />
+        )}
+      </div>
+      <section class="list">
+        <NewMaterial setAlert={setAlert} showAlert={showAlert} />
+        <FindItem />
+        <List items={items} />
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
